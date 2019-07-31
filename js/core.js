369e170d926b161d1fe1a54390e4c887814325e9
Dabomstew <4035468+Dabomstew@users.noreply.github.com>
2019-07-31 17:34:59 +1200

    [BETA] Dark mode + toggler

--- js/core.js
+++ js/core.js
@@ -11,3 +11,73 @@ $(".cardExtraInfo").on('show.bs.collapse', function() {
 $(".cardExtraInfo").on('hide.bs.collapse', function() {
     $("#card"+($(this).attr("data-cardid"))+"Toggler").text("expand_more");
 });
+
+let settingStored = false;
+$(document).ready( function() {
+    if (typeof(Storage) !== "undefined") {
+        // enable toggle
+        if('darkMode' in window.localStorage) {
+            settingStored = true;
+            if(window.localStorage.darkMode === 'yes') {
+                $("html").addClass("dark-mode");
+            }
+        }
+        else {
+            // check if they want dark mode explicitly, else set light mode for now
+            if(window.matchMedia("(prefers-color-scheme: dark)").matches) {
+                $("html").addClass("dark-mode");
+            }
+        }
+
+        $("#darkmode-toggle").click(function() {
+            if(settingStored) {
+                if(window.localStorage.darkMode === 'yes') {
+                    $("#darkmode-preference").html("Your current preference is set to: <b>dark mode</b>");
+                    $("#darkmode-yes").prop("checked", true);
+                }
+                else {
+                    $("#darkmode-preference").html("Your current preference is set to: <b>light mode</b>");
+                    $("#darkmode-no").prop("checked", true);
+                }
+            }
+            else {
+                if(window.matchMedia("(prefers-color-scheme: dark)").matches) {
+                    $("#darkmode-preference").html("Your preference has been autodetected as <b>dark mode</b> based on system settings, you can override it here.");
+                    $("#darkmode-yes").prop("checked", true);
+                }
+                else if(window.matchMedia("(prefers-color-scheme: light)").matches) {
+                    $("#darkmode-preference").html("Your preference has been autodetected as <b>light mode</b> based on system settings, you can override it here.");
+                    $("#darkmode-no").prop("checked", true);
+                }
+                else {
+                    $("#darkmode-preference").html("You are currently being shown <b>light mode</b> as we can't detect a preference from your browser, you can make a choice here.");
+                    $("#darkmode-no").prop("checked", true);
+                }
+            }
+
+            $("#darkmode-modal").modal();
+        });
+
+        $("#darkmode-save-button").click(function() {
+            if($("#darkmode-yes").prop("checked")) {
+                window.localStorage.setItem("darkMode", "yes");
+                $("html").addClass("dark-mode");
+            }
+            else {
+                window.localStorage.setItem("darkMode", "no");
+                $("html").removeClass("dark-mode");
+            }
+            settingStored = true;
+            $("#darkmode-modal").modal('hide');
+        });
+    } else {
+        // query preference and only set dark mode if yes. and make the toggle just an actual toggle that doesn't save anything
+        if(window.matchMedia("(prefers-color-scheme: dark)").matches) {
+            $("html").addClass("dark-mode");
+        }
+
+        $("#darkmode-toggle").click(function() {
+            $("html").toggleClass("dark-mode");
+        });
+    }
+});
