d0404950c29ec79f0143756d216b9010ccb05053
Peter "Marenthyu" Fredebold <petersilie93@live.de>
2020-10-31 23:55:41 +0100

    Remove actions (for now) and close notification after Browser Opens
    Signed-off-by: Peter "Marenthyu" Fredebold <petersilie93@live.de>

--- sw.js
+++ sw.js
@@ -14,9 +14,6 @@ self.addEventListener('push', event => {
     console.log("Push Event received!");
     let data = event.data.json();
     let actions = [];
-    if (data["type"] === "bountyFilled") {
-        actions = [{action: 'open-hand', title: 'Show Hand'}, {action: 'open-nepbot', title: 'Open NepNepBot'}]
-    }
     event.waitUntil(self.registration.showNotification("Waifu TCG", {
         image: data['image'],
         // icon: "https://marenthyu.de/danboorutool/img/nep.png",
@@ -29,4 +26,5 @@ self.addEventListener('push', event => {
 self.addEventListener('notificationclick', function(event) {
     let data = event.notification.data;
     event.waitUntil(clients.openWindow(data['openurl']));
+    event.notification.close();
 });
